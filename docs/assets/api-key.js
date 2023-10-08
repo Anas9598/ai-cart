4d8674a5439e482a9b19724c77b0c429939ed6be
Mohd Anas Khan <samad.27.02.89@gmail.com>
2023-10-08 12:43:28 +0530

    update for api key leak issue

--- docs/assets/api-key.js
+++ docs/assets/api-key.js
@@ -1,4 +1,4 @@
-const apiKey = "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";
+const apiKey = "s@$!;k-6cOje3OYDOC@$!;MoTLL8TFnT3BlbkFJtW0HkYk@$!;nuld0eeb@$!;ScF3o";
 
 function getApiKey() {
     return this.apiKey;
